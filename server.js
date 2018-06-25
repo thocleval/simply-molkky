0eae4e0c0b42d516de7cdfa1238f6673cc649ecd
Valentin Chevalier <valentinchevalier@munso.co>
2018-06-25 17:25:57 +0200

    Configure Heroku and add a litlle node server to serve assets

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
